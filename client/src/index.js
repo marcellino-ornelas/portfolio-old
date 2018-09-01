e89f99b47cb422c04ad31dafbfdadb5ee0022676
marcellino-ornelas <marcellino.ornelas.com>
2018-09-01 09:09:31 -0700

    initailized google analytis

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import createHistory from 'history/createBrowserHistory';
+
+ReactGA.initialize('UA-125057302-1');
 
 // Component level styling
 // import './styles/main.css';
+const history = createHistory();
+
+history.listen((location, action) => {
+  console.log(location);
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
 
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <App />
   </Router>,
   document.getElementById('app')
